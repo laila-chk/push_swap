3e55c222dbedf212258322c5c27dd2de2aaf6520
Laila Chokri <lchokri@e2r10p11.1337.ma>
2022-05-13 14:36:34 +0100

    E2

--- join_all_args.c
+++ join_all_args.c
@@ -1,6 +1,26 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   join_all_args.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/13 10:19:15 by lchokri           #+#    #+#             */
+/*   Updated: 2022/05/13 14:35:41 by lchokri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	check_empty_arg(int i, int c, char *p, char **v)
+{
+	if ((i <= c  && v[i][0] == '\0') || v[1][0] == '\0')
+		{
+			free(*joined_args);
+			write (2, "Error: empty argument!", 22);
+			exit(EXIT_FAILURE);
+		}
+}
 
 void	join_all_args(char **joined_args, char **v, int c)
 {
@@ -9,17 +29,24 @@ void	join_all_args(char **joined_args, char **v, int c)
 
 	i = 2;
 	j = 0;
+	*(joined_args) = ft_calloc(ft_strlen(v[1]), sizeof(char) + 1);
 	while (v[1][j] != '\0')
 	{
-		*joined_args[j] = v[1][j];
+		(*joined_args)[j] = v[1][j];
 		j++;
 	}
-	*joined_args[j] = ' ';
-	*joined_args[j + 1] = '\0';
-	printf("%s", *joined_args);
+	(*joined_args)[j] = ' ';
+	(*joined_args)[j + 1] = '\0';
+	printf("c = %d, i = %d\n", c, i);
 	while (i < c)
 	{
 		*joined_args = ft_strjoin(*joined_args, v[i]);
 		i++;
+		if ((i < c  && v[i][0] == '\0') || v[1][0] == '\0')
+		{
+			free(*joined_args);
+			write (2, "Error: empty argument!", 22);
+			exit(EXIT_FAILURE);
+		}
 	}
 }
--- push_swap.c
+++ push_swap.c
@@ -6,53 +6,29 @@
 /*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 11:30:55 by lchokri           #+#    #+#             */
-/*   Updated: 2022/05/12 15:29:26 by lchokri          ###   ########.fr       */
+/*   Updated: 2022/05/13 14:35:35 by lchokri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-void	join_all_args(char **joined_args, char **v, int c)
-{
-	int	i;
-	int	j;
-
-	i = 2;
-	j = 0;
-	while (v[1][j] != '\0')
-	{
-		(*joined_args)[j] = v[1][j];
-		j++;
-	}
-	(*joined_args)[j] = ' ';
-	(*joined_args)[j + 1] = '\0';
-//	printf("%s", *joined_args);
-	while (i < c)
-	{
-		*joined_args = ft_strjoin(*joined_args, v[i]);
-		i++;
-	}
-}
-
-void	check_args(char *str)
-{ /****holap we gotta check on # so we have to works with atoi first ****/
+/*void	check_args(char *str)
+{ 
 	while (*str)
 	{
 		if (str)
-	}
-}
-
+*/
 int main(int c, char **v)
 {
 	char	*joined_args;
 
-	joined_args = ft_calloc(ft_strlen(v[1]), sizeof(char) + 1);
-	if (c <= 1)
-		write(2, "too few arguments\n", 18);
+	if (c < 2)
+		write(2, "Error: too few arguments!\n", 27);
 	else 
 	{
-		join_all_args(&joined_args, v, c);
-		printf("%s", joined_args);
+			join_all_args(&joined_args, v, c);
+			printf("%s\n", joined_args);
+			free(joined_args);
 	}
 }
