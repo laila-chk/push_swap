1d1d59c6ccb46952b9efe971dac81b0957a31570
laila-chk <lyly.2356@gmail.com>
2022-05-18 17:39:57 +0100

    e3

--- operations.c
+++ operations.c
@@ -38,23 +38,61 @@ void ft_push(int *i, int *j, int *len) //len is either b_len or a_len, depends o
 	(*len)++;
 }
 
-void	rotate(int *i, int len)
+int	rotate(int *i, int len)
 {
 	int		j;
 	int		k;
 
 	j = 0;
 	k = i[0];
-	while(j < len)
-	{
+	while(j < len/* and len > 1*/)
 		i[j] = i[j + 1];
 		j++;
 	}
 	i[len - 1] = k;
 }
 
-void	sa(int *i, int len)
+void	sa(int *a, int a_len)
 {
-	ft_swap(i, len);
+	ft_swap(a, a_len);
 	write(1, "sa\n", 3);
 }
+
+void	sb(int *b, int b_len)
+{
+	ft_swap(b, b_len);
+	write(1, "sb\n", 3);
+}
+
+void	pa(int *a, int *b, int *a_len, int b_len)
+{
+	if (b_len > 0)
+	{
+		ft_push(b, a, a_len);
+		write(1, "ra\n", 3);
+	}
+}
+
+void	pb(int *b, int *a, int *b_len, int a_len)
+{
+	if (a_len > 0)
+	{
+		ft_push(a, b, b_len);
+		write(1, "ra\n", 3);
+	}
+}
+
+void	ra(int *a, int a_len)
+{
+	rotate(a, a_len);
+	write(1, "ra\n", 3);
+}
+
+void	rb(int *b, int b_len)
+{
+	rotate(b, b_len);
+	//we print only when rotation actually happens..
+	//in my func 'rotate' i do rotate even if len is one, we need\
+	//to add an if condition there i guess
+}
+
