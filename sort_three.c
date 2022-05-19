aa87e45e221fa8406dea263972ab0505bbefb347
Laila Chokri <lchokri@e2r8p3.1337.ma>
2022-05-19 16:56:57 +0100

    E2 wed midnight

--- sort_three.c
+++ sort_three.c
@@ -6,7 +6,7 @@
 /*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 22:46:39 by lchokri           #+#    #+#             */
-/*   Updated: 2022/05/19 00:39:20 by lchokri          ###   ########.fr       */
+/*   Updated: 2022/05/19 16:12:23 by lchokri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,42 @@
 void	doubled_elm(void)
 {
 	write(2, "Error: element is dooubled!\n", 29);
-	// Do i need to free a? 
 	exit(EXIT_FAILURE);
+}
+
+void	check_for_two(int *a, int a_len)
+{
+	if (a_len == 2)
+	{
+		if (a[0] > a[1])
+			sa(a, a_len);
+		if (a[0] == a[1])
+			doubled_elm();
+	}
 }
 
 void	sort_three(int *a, int a_len)
 {
-	int		i;
-	int		j;
-
-	j = 0;
-	i = 0;
-	if (a_len == 2)
+	check_for_two(a, a_len);
+	if (a_len == 3)
 	{
-		if (a[0] < a[1])
-			sa(a, a_len);
-		else if (a[0] == a[1])
+		if (a[0] == a[1] || a[0] == a[2] || a[1] == a[2])
 			doubled_elm();
-	}
-	else if (a_len == 3)
-	{
-		// i think i'll push smalest elem into b, call the case of len =2 and pa angain
-	}
+		if (a[0] > a[1] && a[0] > a[2])
+		{
+			ra(a, a_len);
+			if (a[0] > a[1])
+				sa(a, a_len);
+		}
+		else if (a[0] > a[1] && a[0] < a[2])
+			sa(a, a_len);
+		else if (a[1] > a[0] && a[1] > a[2])
+		{
+			if (a[0] < a[2])
+				sa(a, a_len);
+			else
+				ra(a, a_len);
+			ra(a, a_len);
+		}
+	}	
 }
